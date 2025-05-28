e0fdaa4c3ad78dd8eabd3ab82455c1e9e2422856
Astna17 <hei.antsa.17@gmail.com>
2025-05-28 09:04:29 +0300

    feat: create type of drink in enum

---
+++ Machine/src/CoffeeType.java
@@ -0,0 +1,24 @@
+public enum CoffeeType {
+    ESPRESSO("C2", "Espresso", 4),
+    LATTE("C3", "Latte", 2),
+    CAPPUCCINO("C4", "Cappuccino", 3);
+
+    private final String codeCoffeeType;
+    private final String coffeeName;
+    private final float price;
+    CoffeeType(String codeCoffeeType, String coffeeName, float price) {
+        this.codeCoffeeType = codeCoffeeType;
+        this.coffeeName = coffeeName;
+        this.price = price;
+    }
+
+    public String getCodeCoffeeType() {
+        return codeCoffeeType;
+    }
+    public String getCoffeeName() {
+        return coffeeName;
+    }
+    public float getPrice() {
+        return price;
+    }
+}
